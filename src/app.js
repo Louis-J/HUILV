b5a5c6de31cad5514bca4e5a4634f11453211d0f
czzcy3832515@gmail.com <czzcy3832515@gmail.com>
2020-09-26 08:03:31 +0800

    更改项目结构目录，将源码单独放到一个目录 引入WEUI，便于使用日历等组件 三个页面内容简单绘制

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
-//app.js
+require('./libs/Mixins.js');
+
+const themeListeners = [];
+
 App({
+  globalData: {
+      theme: 'light', // dark
+  },
+  themeChanged(theme) {
+      this.globalData.theme = theme;
+      themeListeners.forEach((listener) => {
+          listener(theme);
+      });
+  },
+  watchThemeChange(listener) {
+      if (themeListeners.indexOf(listener) < 0) {
+          themeListeners.push(listener);
+      }
+  },
+  unWatchThemeChange(listener) {
+      const index = themeListeners.indexOf(listener);
+      if (index > -1) {
+          themeListeners.splice(index, 1);
+      }
+  },
+
+  
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
